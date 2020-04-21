c7639f7f6203fa03fd86b9b0b350fcba29677373
Cierra Johnson-carter <cierrajohnson-carter@Cierras-Air.lan1>
2020-04-21 18:54:51 -0400

    Fixed iteration bug

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ What would you like to do?
 
 selected_input = input(menu_prompt).strip().lower()
 
-# Takes book info and adds it to a reading list
+# Takes book info
 
 
 def add_book():
@@ -18,25 +18,37 @@ def add_book():
     author = input('Author: ').strip().title()
     year = input('Year Published: ').strip()
 
+# Adds book to reading list
     reading_list.append({
-        'title': title, 'author': author, 'year': year})
+        'title': title,
+        'author': author,
+        'year': year
+    })
 
 # Lists the books in the reading list
 
 
 def display_book():
+    print()
+
     for book in reading_list:
         title, author, year = book.values()
         print(f'{title}, by{author} ({year})')
 
+    print()
 
-# Runs 'add_book' and 'display_book' depending on user input and lets the user know if the reading list is empty
-while True:
-    if selected_input == 'q':
-        break
-    elif selected_input == 'a':
+
+# Runs 'add_book' and 'display_book' depending on user input and lets the user know if the reading list is empty or if they didn't enter a valid input
+while selected_input != 'q':
+    if selected_input == 'a':
         add_book()
     elif selected_input == 'l':
-        display_book()
+        if reading_list:
+            display_book()
+        else:
+            print('Your reading list is empty')
     else:
-        print('Your reading list is empty.')
+        print('Sorry,your input is not valid')
+
+# Allows user to change selection after each iteration
+    selected_input = input(menu_prompt).strip().lower()
